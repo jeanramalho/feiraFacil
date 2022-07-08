041e3d09d213fff6baffa661690400d19a1e662b
jeanramalho <jeanramalho.dev@gmail.com>
2022-07-08 17:01:36 -0300

    Inicia criação do layout do projeto

--- js/script.js
+++ js/script.js
@@ -6,7 +6,3 @@ db.transaction(function(tx){
     tx.executeSql('CREATE TABLE feira ( ID INTEGER PRIMARY KEY, item TEXT, quantidade INTEGER, valor INTEGER)')
 })
 
-function ready() {
-    document.getElementById('salvar').addEventListener('click', salvar)
-    mostrar()
-}
