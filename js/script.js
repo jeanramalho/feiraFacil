368cb241768d59a4345c7547b72d88787d2058cc
jeanramalho <jeanramalho.dev@gmail.com>
2022-07-08 13:56:14 -0300

    Implementa comandos de banco de dados

---
+++ js/script.js
@@ -0,0 +1,12 @@
+window.addEventListener('load', ready)
+
+var db = openDatabase('feiraFacil', '1.0', 'Feira Database', 2 * 1024 + 1024)
+
+db.transaction(function(tx){
+    tx.executeSql('CREATE TABLE feira ( ID INTEGER PRIMARY KEY, item TEXT, quantidade INTEGER, valor INTEGER)')
+})
+
+function ready() {
+    document.getElementById('salvar').addEventListener('click', salvar)
+    mostrar()
+}
